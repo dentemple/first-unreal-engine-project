83a37fb8c575d4643780e9a4a5bd4d62fc425666
Denny Temple <dev@dentemple.com>
2020-08-25 00:13:53 -0400

    Initial commit

---
+++ Source/FirstUnrealProject/FirstUnrealProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstUnrealProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFirstUnrealProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFirstUnrealProjectGameMode();
+};
+
+
+
